8320f096895709441a97797f1fc23de911bd4f2f
davidgaviriatatis <david.gaviria.tatis@gmail.com>
2023-11-27 11:04:11 -0500

    commit inicial

---
+++ src/main/java/com/loadfilesservice/loadfiles/LoadfilesApplication.java
@@ -0,0 +1,13 @@
+package com.loadfilesservice.loadfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadfilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loadfilesservice/loadfiles/LoadfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loadfilesservice.loadfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadfilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
